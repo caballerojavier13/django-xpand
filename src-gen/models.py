7568c2a8ddf85777dd5bdcf50fb687a62a55f79e
adamantike <adamantike@users.noreply.github.com>
2014-10-28 17:53:04 -0300

    Implementación inicial de la plantilla Xpand para models.py

---
+++ src-gen/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+        
+
+class Persona(models.Model):
+    # Atributos
+    dni = Model.IntegerField()
+    nombre = Model.CharField(max_length=50)
+    apellido = Model.CharField(max_length=50)
+    # Relaciones
+    domicilio = Model.ForeignKey('Domicilio')
+
+class Domicilio(models.Model):
+    # Atributos
+    calle = Model.CharField(max_length=50)
+    numero = Model.IntegerField()
+    localidad = Model.CharField(max_length=50)
+    # Relaciones
+    
