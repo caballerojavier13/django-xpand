326ca219751eb6e6c667a4e8838fcd406d0769a0
adamantike <adamantike@users.noreply.github.com>
2014-10-28 18:01:05 -0300

    Implementación inicial de la plantilla Xpand para admin.py

---
+++ src-gen/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from .models import Persona,Domicilio
+
+admin.site.register(Persona)
+admin.site.register(Domicilio)
+    
--- src-gen/models.py
+++ src-gen/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-        
+
 
 class Persona(models.Model):
     # Atributos
