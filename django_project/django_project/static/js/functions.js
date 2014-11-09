5e8cb5601235490b7b33e1bab848adda31ddef9e
adamantike <adamantike@users.noreply.github.com>
2014-11-08 22:00:38 -0300

    Inclusión de archivos estáticos desde MDA2014/django-abm al proyecto Django

---
+++ django_project/django_project/static/js/functions.js
@@ -0,0 +1,36 @@
+var miniLoading = false;
+
+$(function(){
+  if($("#listado_modulos li").length < 1){
+    $("#listado_modulos").parent().remove();
+  }
+  $(".alert .close").on("click",function(){
+      $(".alert").hide();
+  });
+});
+
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+function getURL_Id(){
+  var urlAux = window.location.pathname.split("/")
+  return urlAux[urlAux.length - 2];
+}
+
+function showMiniLoading(){
+  miniLoading = true;
+  $("body").append('<svg class="spinner" width="65px" height="65px" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg"><circle class="path" fill="none" stroke-width="3" stroke-linecap="round" cx="33" cy="33" r="30"></circle></svg>');
+}
+function hideMiniLoading(){
+  if( miniLoading ){
+    setTimeout(
+        function() {
+          $(".spinner").remove();  
+      }, 2000);
+    
+  }
+}
