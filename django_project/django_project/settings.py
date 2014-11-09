dab0c2a026724360da4ac93367fd33735e999cee
adamantike <adamantike@users.noreply.github.com>
2014-11-08 22:42:45 -0300

    Corrección de archivos estáticos JS del proyecto Django

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -84,7 +84,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, os.path.dirname(__file__), "static"),
+    os.path.join(BASE_DIR, os.path.dirname(__file__), 'static'),
 )
 
 TEMPLATE_DIRS = {
---
+++ django_project/django_project/static/js/funciones_tabla.js
@@ -0,0 +1,29 @@
+function cargarTabla(idTabla, data, callback) {
+    $(idTabla + " tbody").empty();
+    if (data.length > 0) {
+        $.each(data, function(index, elemento) {
+            $(idTabla + " tbody").append(elemento);
+        });
+    }
+    else {
+        tablaVacia(idTabla, "100%");
+    }
+	if (callback) {
+	    callback();
+	}
+}
+
+function generarTabla(url, callback){
+    var tabla = new Array();
+    $.getJSON(url, function(data) {
+	    jQuery.each(data,function(index, elemento) {
+	        var fila = armarFila(index, elemento);
+	        tabla.push(fila);
+	    });
+	    callback(tabla);
+	});
+}
+
+function tablaVacia(idTabla, cantidadColumnas){
+    $(idTabla + " tbody").html('<tr><td class="text-center" colspan="'+ cantidadColumnas +'">No se ha encontrado ningún elemento.</td></tr>');
+}
